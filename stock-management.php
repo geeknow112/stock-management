97a2a5d3e04ef11761a37bcce027d9a0804d7d4a
przorzcrz <przorzcrz@gmail.com>
2023-07-16 16:37:12 +0000

    fix sales-detail regist action.

--- stock-management.php
+++ stock-management.php
@@ -203,7 +203,63 @@ class StockManagement {
 	 **/
 	function sales_detail() {
 		$blade = $this->set_view();
-		echo $blade->run("sales-detail");
+		$get = (object) $_GET;
+		$post = (object) $_POST;
+$this->vd($post);
+
+		switch($get->action) {
+			case 'regist':
+				$tb = new Sales;
+				break;
+
+			case 'save':
+				if (!empty($_POST)) {
+					$get = (object) $_POST;
+					if ($get->cmd == 'save') {
+						$get->messages = array('error' => array('error is _field_company-name.')); // TEST DATA 
+						$tb = new Sales;
+						$rows = $tb->updDetail($prm);
+
+					}
+					if (empty($get->messages)) {
+	//					$result = $tb->updShopDetail($prm, $p);
+					} else {
+						echo '<script>var msg = document.getElementById("msg"); msg.innerHTML = "'. $post->messages['error'][0]. '";</script>';
+					}
+				}
+				$formPage = 'sales-list';
+				echo $blade->run("sales-detail", compact('rows', 'formPage', 'prm'));
+				break;
+
+			case 'edit-exe':
+				$get = (object) $_GET;
+				$post = (object) $_POST;
+				$tb = new Sales;
+				if (!empty($_POST)) {
+					if ($post->cmd == 'save') {
+						$post->messages = array('error' => array('error is _field_company-name.')); // TEST DATA 
+$msg = $this->getValidMsg();		
+$this->vd($msg);
+						if ($msg['msg'] != 'success') {
+						} else {
+							$rows = $tb->updDetail($get, $post);
+						}
+
+					}
+					if (empty($post->messages)) {
+					} else {
+						echo '<script>var msg = document.getElementById("msg"); msg.innerHTML = "'. $post->messages['error'][0]. '";</script>';
+					}
+				}
+				
+				$rows = $tb->getDetail($get);
+
+				$formPage = 'sales-list';
+				echo $blade->run("sales-detail", compact('rows', 'formPage', 'get', 'post', 'msg'));
+
+				break;
+
+		}
 	}
 
 	/**
