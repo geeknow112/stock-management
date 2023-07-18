af7a131da8716f75ab4f86764e34108434c0f0ed
przorzcrz <przorzcrz@gmail.com>
2023-07-18 07:27:19 +0000

    fix sales-detail add select parts.

--- views/layout/parts_order_regist.blade.php
+++ views/layout/parts_order_regist.blade.php
@@ -10,52 +10,82 @@
 	@endif
 
 	<div class="row mb-3">
+		<label for="orderName" class="col-sm-2 col-form-label">氏名</label>
+		<select class="form-select" aria-label="orderName">
+			@foreach($initForm['select']['order_name'] as $i => $d)
+				<option value="{{$i}}" @if ($i == $rows->name) selected @endif >{{$d}}</option>
+			@endforeach
+		</select>
+<!--		<div id="orderName" class="form-text">氏名を入力してください。</div>-->
+	</div>
+
+	<div class="row mb-3">
 		<label for="carModel" class="col-sm-2 col-form-label">車種</label>
-	<select class="form-select" aria-label="carModel">
-		@foreach($initForm['select']['car_model'] as $i => $d)
-			<option value="{{$i}}" @if ($i == $rows->class) selected @endif >{{$d}}</option>
-		@endforeach
-	</select>
+		<select class="form-select" aria-label="carModel">
+			@foreach($initForm['select']['car_model'] as $i => $d)
+				<option value="{{$i}}" @if ($i == $rows->class) selected @endif >{{$d}}</option>
+			@endforeach
+		</select>
 	</div>
 
 	<div class="row mb-3">
 		<label for="goodsName" class="col-sm-2 col-form-label">品名</label>
-	<select class="form-select" aria-label="goodsName">
-		@foreach($initForm['select']['goods_name'] as $i => $d)
-			<option value="{{$i}}" @if ($i == $rows->goods) selected @endif >{{$d}}</option>
-		@endforeach
-	</select>
+		<select class="form-select" aria-label="goodsName">
+			@foreach($initForm['select']['goods_name'] as $i => $d)
+				<option value="{{$i}}" @if ($i == $rows->goods) selected @endif >{{$d}}</option>
+			@endforeach
+		</select>
 	</div>
 
 	<div class="row mb-3">
-		<label for="shipAdd" class="col-sm-2 col-form-label">配送先</label>
-		<input type="text" class="col-sm-6 col-form-control" id="ship_addr" name="ship_addr" aria-describedby="shipAddHelp" value="{{$rows->ship_addr}}">
+		<label for="shipAddr" class="col-sm-2 col-form-label">配送先</label>
+		<select class="form-select" aria-label="shipAddr">
+			@foreach($initForm['select']['ship_addr'] as $i => $d)
+				<option value="{{$i}}" @if ($i == $rows->ship_addr) selected @endif >{{$d}}</option>
+			@endforeach
+		</select>
 <!--		<div id="shipAddHelp" class="form-text">配送先を入力してください。</div>-->
 	</div>
 
 	<div class="row mb-3">
 		<label for="qty" class="col-sm-2 col-form-label">量(t)</label>
-		<input type="text" class="col-sm-6 col-form-control" id="qty" name="qty" aria-describedby="qtyHelp" value="{{$rows->qty}}">
+		<select class="form-select" aria-label="qty">
+			@foreach($initForm['select']['qty'] as $i => $d)
+				<option value="{{$i}}" @if ($i == $rows->qty) selected @endif >{{$d}}</option>
+			@endforeach
+		</select>
 <!--		<div id="qtyHelp" class="form-text">量(t)を入力してください。</div>-->
 	</div>
 
 	<div class="row mb-3">
-		<label for="arrivalDt" class="col-sm-2 col-form-label">入庫予定日</label>
+		<label for="use_stock" class="col-sm-2 col-form-label">在庫から配送</label>
+		<input type="checkbox" class="col-sm-2 form-check-input" id="use_stock">
+	</div>
+
+	<div class="row mb-3">
+		<label for="arrival_dt" class="col-sm-2 col-form-label">入庫予定日</label>
 		<input type="date" class="col-sm-6 col-form-control" id="arrival_dt" name="arrival_dt" aria-describedby="arrivalDtHelp" value="{{$rows->arrival_dt}}">
 <!--		<div id="arrivalDtHelp" class="form-text">入庫予定日を入力してください。</div>-->
 	</div>
 
 	<div class="row mb-3">
-		<label for="orderName" class="col-sm-2 col-form-label">氏名</label>
-		<input type="text" class="col-sm-6 col-form-control" id="order_name" name="order_name" aria-describedby="orderNameHelp" value="{{$rows->name}}">
-<!--		<div id="orderName" class="form-text">氏名を入力してください。</div>-->
+		<label for="ship_dt" class="col-sm-2 col-form-label">配送予定日</label>
+		<input type="date" class="col-sm-6 col-form-control" id="ship_dt" name="ship_dt" aria-describedby="shipDtHelp" value="{{$rows->ship_dt}}">
+<!--		<div id="arrivalDtHelp" class="form-text">入庫予定日を入力してください。</div>-->
 	</div>
 
+	<div class="row mb-3">
+		<label for="outgoing_warehouse" class="col-sm-2 col-form-label">出庫倉庫</label>
+		<select class="form-select" aria-label="outgoing_warehouse">
+			@foreach($initForm['select']['outgoing_warehouse'] as $i => $d)
+				<option value="{{$i}}" @if ($i == $rows->outgoing_warehouse) selected @endif >{{$d}}</option>
+			@endforeach
+		</select>
+	</div>
 
-
-	<div class="row mb-3 form-check">
-		<input type="checkbox" class="form-check-input" id="repeat">
-		<label class="form-check-label" for="repeat">繰り返し予定を設定する</label>
+	<div class="row mb-3">
+		<label for="repeat" class="col-sm-2 col-form-label">繰り返し予定を設定する</label>
+		<input type="checkbox" class="col-sm-2 form-check-input" id="repeat">
 	</div>
 
 <br /><br /><hr>
