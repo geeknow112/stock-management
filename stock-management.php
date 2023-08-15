6217a637a0f281c2778f14685d663fc4521feef6
przorzcrz <przorzcrz@gmail.com>
2023-08-15 05:37:13 +0000

    fxi SalesController

--- stock-management.php
+++ stock-management.php
@@ -153,81 +153,8 @@ class StockManagement {
 	 *
 	 **/
 	function sales_detail() {
-		$blade = $this->set_view();
-		$get = (object) $_GET;
-		$post = (object) $_POST;
-//$this->vd($post);
-
-		switch($get->action) {
-			default:
-				$tb = new Sales;
-				$initForm = $tb->getInitForm();
-				$formPage = 'sales-list';
-				echo $blade->run("sales-detail", compact('formPage', 'get', 'initForm'));
-				break;
-
-			case 'regist':
-				$tb = new Sales;
-				break;
-
-			case 'save':
-				if (!empty($_POST)) {
-					$get = (object) $_POST;
-					if ($get->cmd == 'save') {
-						$get->messages = array('error' => array('error is _field_company-name.')); // TEST DATA 
-						$tb = new Sales;
-						$rows = $tb->updDetail($prm);
-
-					}
-					if (empty($get->messages)) {
-	//					$result = $tb->updShopDetail($prm, $p);
-					} else {
-						echo '<script>var msg = document.getElementById("msg"); msg.innerHTML = "'. $post->messages['error'][0]. '";</script>';
-					}
-				}
-				$formPage = 'sales-list';
-				echo $blade->run("sales-detail", compact('rows', 'formPage', 'prm'));
-				break;
-
-			case 'edit':
-				$tb = new Sales;
-				$initForm = $tb->getInitForm();
-				$rows = $tb->getDetail($get);
-				$post = $rows;
-				$formPage = 'sales-list';
-				echo $blade->run("sales-detail", compact('rows', 'formPage', 'get', 'post', 'initForm'));
-				break;
-
-			case 'edit-exe':
-				$get = (object) $_GET;
-				$post = (object) $_POST;
-//$this->vd($post);
-				$tb = new Sales;
-				if (!empty($_POST)) {
-					if ($post->cmd == 'save') {
-						$post->messages = array('error' => array('error is _field_company-name.')); // TEST DATA 
-$msg = $this->getValidMsg();		
-//$this->vd($msg);
-						if ($msg['msg'] != 'success') {
-						} else {
-							$rows = $tb->updDetail($get, $post);
-						}
-
-					}
-					if (empty($post->messages)) {
-					} else {
-						echo '<script>var msg = document.getElementById("msg"); msg.innerHTML = "'. $post->messages['error'][0]. '";</script>';
-					}
-				}
-				
-				$rows = $tb->getDetail($get);
-//$this->vd($rows);
-				$formPage = 'sales-list';
-				echo $blade->run("sales-detail", compact('rows', 'formPage', 'get', 'post', 'msg'));
-
-				break;
-
-		}
+		$s = new SalesController();
+		$s->detailAction();
 	}
 
 	/**
