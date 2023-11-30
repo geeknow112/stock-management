a414f376742e86b966445fb3afca9564d292d342
przorzcrz <przorzcrz@gmail.com>
2023-11-30 07:08:27 +0000

    add stock-list.

--- views/stock-list.blade.php
+++ views/stock-list.blade.php
@@ -3,7 +3,7 @@
 
 <div id="wpbody-content">
 	<div class="wrap">
-		<h1 class="wp-heading-inline">【在庫管理】</h1>
+		<h1 class="wp-heading-inline">【在庫検索】</h1>
 		<!--<a href="<?php echo home_url(); ?>/wp-admin/admin.php?page={{$formPage}}&action=regist" name="cmd_regist" id="cmd_regist" class="page-title-action">新規登録</a>-->
 		<!--<a href="<?php echo home_url(); ?>/wp-admin/admin.php?page=agreement" name="cmd_regist" id="cmd_regist" class="page-title-action">新規登録</a>-->
 
@@ -12,107 +12,42 @@
 		<form name="forms" id="forms" action="" method="" enctype="multipart/form-data">
 {{--			@if ($tb->getCurUser()->roles[0] == 'administrator')	--}}
 			<div class="search-box">
+				<label for="goods" class="col-sm-2 col-form-label">商品番号：</label>
+					<input type="search" id="goods" name="s[no]" value="<?php echo htmlspecialchars($get->s['no']); ?>"><br /><br />
 
-				<label for="carModel" class="col-sm-2 col-form-label">引取(入庫)予定日：</label>
-					<input type="date" id="user-search-input" name="s[arrival_s_dt]" value="<?php echo htmlspecialchars($g['s']['arrival_s_dt']); ?>" placeholder="2020-11-01"><!--&emsp;～&emsp;
-				<input type="date" id="user-search-input" name="s[arrival_e_dt]" value="<?php echo htmlspecialchars($g['s']['arrival_e_dt']); ?>" placeholder="2022-12-01">&emsp;--><br /><br />
+				<label for="goods_name" class="col-sm-2 col-form-label">商品名：</label>
+					<input type="search" id="goods_name" name="s[goods_name]" value="<?php echo htmlspecialchars($get->s['goods_name']); ?>"><br /><br />
+
+				<label for="qty" class="col-sm-2 col-form-label">荷姿・容量：</label>
+					<input type="search" id="qty" name="s[qty]" value="<?php echo htmlspecialchars($get->s['qty']); ?>"><br /><br />
+
+				<label for="lot" class="col-sm-2 col-form-label">ロット番号：</label>
+					<input type="search" id="lot" name="s[lot]" value="<?php echo htmlspecialchars($get->s['lot']); ?>"><br /><br />
 
 				<label for="carModel" class="col-sm-2 col-form-label">出庫倉庫：</label>
 					<input type="search" id="user-search-input" name="s[outgoing_warehouse]" value="<?php echo htmlspecialchars($g['s']['outgoing_warehouse']); ?>" disabled>&emsp;&emsp;&emsp;&emsp;
 
 				<input type="button" id="search-submit" class="btn btn-primary" onclick="cmd_search();" value="検索">
 
+				<script>
+				function cmd_search() {
+					document.forms.method = 'get';
+					document.forms.action = "/wp-admin/admin.php?page=stock-list&sales={{$get->sales}}&goods={{$get->goods}}&action=search"
+					document.forms.cmd.value = 'search';
+					document.forms.submit();
+				}
+				</script>
 			</div>
-			<br />
-
 			<input type="hidden" id="_wpnonce" name="_wpnonce" value="5647b2c250">
 			<!--<input type="hidden" name="_wp_http_referer" value="/wp-admin/users.php">-->
-			<input type="hidden" name="page" value="sales-list">
+			<input type="hidden" name="page" value="stock-list">
 			<input type="hidden" name="action" value="search">
 			<input type="hidden" name="cmd" value="">
 {{--			@endif	--}}
 
 
-{{-- $wp_list_table->display() --}}
-<!-- start -->
-<div id="wpbody-content">
-	<div class="wrap">
+{{ $wp_list_table->display() }}
 
-		<form method="get">
-			<div class="tablenav top">
-				<br class="clear">
-			</div>
-			
-		<div class="table-responsive">
-
-		<div>
-			<b>日時：2022-12-**</b>
-			<table class="table table-bordered text-nowrap">
-				<thead class="table-light">
-					<tr>
-						<th class="">コード</th>
-						<th class="">品名</th>
-						<th class="">容量(kg)</th>
-						<th class="">個数</th>
-						<th class="">重量(kg)</th>
-						<th class="">ロット番号</th>
-						<th class="">備考</th>
-						<th class="">適用</th>
-					</tr>
-				</thead>
-
-				<tbody id="the-list" data-wp-lists="list:user">
-					<tr>
-						<td class=""><input type="text" value="43530"></td>
-						<td class=""><input type="text" value="すこぶる育成　カミ"></td>
-						<td class=""><input type="text" value="20"></td>
-						<td class=""><input type="text" value="35"></td>
-						<td class=""><input type="text" value="700"></td>
-						<td class=""><input type="text" value=""></td>
-						<td class=""><input type="text" value="パレット"></td>
-						<td class=""><input type="text" value=""></td>
-					</tr>
-					<tr>
-						<td class=""><input type="text" value="43565"></td>
-						<td class=""><input type="text" value="繁殖用マミー　カミ"></td>
-						<td class=""><input type="text" value="20"></td>
-						<td class=""><input type="text" value="15"></td>
-						<td class=""><input type="text" value="300"></td>
-						<td class=""><input type="text" value=""></td>
-						<td class=""><input type="text" value="パレット"></td>
-						<td class=""><input type="text" value=""></td>
-					</tr>
-					<tr>
-						<td class=""><input type="text" value="49120"></td>
-						<td class=""><input type="text" value="みやび後期　カミ"></td>
-						<td class=""><input type="text" value="20"></td>
-						<td class=""><input type="text" value="50"></td>
-						<td class=""><input type="text" value="1,000"></td>
-						<td class=""><input type="text" value=""></td>
-						<td class=""><input type="text" value="パレット"></td>
-						<td class=""><input type="text" value=""></td>
-					</tr>
-
-{{--
-			@if (isset($rows) && count($rows))
-				<tbody id="the-list" data-wp-lists="list:user">
-					@foreach ($rows as $delivery_dt => $list)
-					<tr id="user-1">
-						<td>
-						</td>
-					</tr>
-					@endforeach
-			@else
-				<td class="colspanchange" colspan="7">検索対象は見つかりませんでした。</td>
-			@endif
---}}
-				</tbody>
-
-				<tfoot>
-				</tfoot>
-			</table>
-</div>
-<!-- end -->
 			<div class="tablenav top">
 				<br class="clear">
 			</div>
