99985501b8c0102bc6c89c9deeda0d7a71b36ebd
przorzcrz <przorzcrz@gmail.com>
2023-12-01 04:48:23 +0000

    add Base.php.

---
+++ models/Stock.php
@@ -0,0 +1,293 @@
+<?php
+/**
+ * Stock.php short discription
+ *
+ * long discription
+ *
+ */
+require_once(dirname(__DIR__). '/library/Ext/Model/Base.php');
+/**
+ * StockClass short discription
+ *
+ * long discription
+ *
+ */
+class Stock extends Ext_Model_Base {
+	protected $_name = 'yc_stock';
+
+	/**
+	 *
+	 **/
+	function getTableName() {
+		return $this->_name;
+	}
+
+	/**
+	 *
+	 **/
+	public function getCurUser() {
+		$cur_user = wp_get_current_user();
+		return $cur_user;
+	}
+
+	/**
+	 * 
+	 **/
+	public function getValidElement($step_num = null) {
+
+		$step1 = array(
+			'rules' => array(
+				'goods_name'				=> 'required|max:100',
+				'qty'						=> 'required|max:100',
+/*
+				'apply_service'				=> 'required|max:100',
+				'apply_plan'				=> 'required|max:100',
+
+				'biz_fg'					=> 'required|max:100',
+				'biz_number'				=> 'required|regex:/^[0-9]{13}+$/i',
+				'company_name'				=> 'required|max:100',
+				'company_name_kana'			=> 'required|max:100|regex:/^[ァ-ヶｦ-ﾟー]+$/u',
+				'zip'						=> 'required|max:100',
+				'pref'						=> 'required|max:100',
+				'addr'						=> 'required|max:100',
+				'addr2'						=> 'required|max:100',
+				'addr3'						=> 'max:100',
+				'addr_kana'					=> 'required|max:100|regex:/^[ァ-ヶｦ-ﾟー]+$/u',
+				'tel'						=> 'required|max:100',
+				'fax'						=> 'max:100',
+				'est_dt'					=> 'required|max:100',
+				'num_employ'				=> 'required|max:100',
+				'capital'					=> 'required|max:100',
+				'annual_sales'				=> 'max:100',
+				'goods_class'				=> 'required|max:100',
+				'goods'						=> 'required|max:100',
+				'delivery_company'			=> 'max:100',
+				'url'						=> 'max:100',
+
+				'name'                  => 'required|max:2',
+				'email'                 => 'required|email',
+				'password'              => 'required|min:6',
+				'confirm_password'      => 'required|same:password',
+				'avatar'                => 'required|uploaded_file:0,500K,png,jpeg',
+				'skills'                => 'array',
+				'skills.*.id'           => 'required|numeric',
+				'skills.*.percentage'   => 'required|numeric'
+*/
+			), 
+			'messages' => array(
+				'name.required' => 'ユーザー名を入力してください',
+				'name.string' => '正しい形式で入力してください',
+				'name.max' => '文字数をオーバーしています。',
+				'email.required' => 'メールアドレスを入力してください。',
+				'email.email' => '正しい形式でメールアドレスを入力してください',
+				'email.max' => '文字数をオーバーしています。',
+				'email.unique' => '登録済みのユーザーです',
+				'password.required' => 'パスワードを入力してください',
+				'password.min' => 'パスワードは8文字以上で入力してください。',
+				'password.confirmed' => 'パスワードが一致しません。',
+			)
+		);
+
+		return $step1;
+	}
+
+	/**
+	 * 商品情報一覧取得
+	 **/
+	public function getList($get = null) {
+		$get = (object) $get;
+		global $wpdb;
+		$cur_user = wp_get_current_user();
+
+		$sql  = "SELECT g.*, g.name AS goods_name ";
+		$sql .= "FROM yc_goods AS g ";
+		$sql .= "WHERE g.goods is not null ";
+
+		if (current($cur_user->roles) != 'administrator') {
+//			$sql .= "AND ap.mail = '". $cur_user->user_email. "'";
+		}
+
+		if (empty($get->action)) {
+//			$sql .= "ORDER BY ap.rgdt desc";
+			$sql .= ";";
+
+		} else {
+			if ($get->action == 'search') {
+				if (!empty($get->s['no'])) { $sql .= sprintf("AND g.goods = '%s' ", $get->s['no']); }
+				if (!empty($get->s['goods_name'])) { $sql .= sprintf("AND g.name LIKE '%s%s' ", $get->s['goods_name'], '%'); }
+//				$sql .= "ORDER BY g.goods desc";
+				$sql .= ";";
+
+			} else {
+//				$sql .= "AND ap.applicant = '". $prm->post. "';";
+			}
+		}
+		$rows = $wpdb->get_results($sql);
+		return $rows;
+	}
+
+	/**
+	 * 商品情報詳細取得
+	 **/
+	public function getDetail($get = null) {
+		$get = (object) $get;
+		global $wpdb;
+		$cur_user = wp_get_current_user();
+
+		// 商品IDで検索して商品情報を取得するSQL
+		$sql  = "SELECT s.* FROM yc_sales as s ";
+		$sql .= "WHERE s.id = '". $get->sales. "'";
+
+		if (current($cur_user->roles) != 'administrator') {
+//			$sql .= "AND ap.mail = '". $cur_user->user_email. "'";
+		}
+
+		$sql .= "LIMIT 1;";
+		$rows = $wpdb->get_results($sql);
+
+/*
+		// 配列整形
+		foreach ($rows as $i => $d) {
+			$ret[str_replace('-', '_', $d->meta_key)] = $d->meta_value;
+		}
+*/
+		return $rows[0];
+	}
+
+	/**
+	 * 商品情報詳細取得
+	 * - 商品コード(goods)から抽出
+	 **/
+	public function getDetailByGoodsCode($goods = null) {
+		global $wpdb;
+
+		$sql  = "SELECT g.* FROM ". $this->getTableName(). " as g ";
+		$sql .= sprintf("WHERE g.goods = '%s' ", $goods);
+		$sql .= "LIMIT 1;";
+
+		$rows = $wpdb->get_results($sql);
+		return $rows[0];
+	}
+
+	/**
+	 * 対象注文のロット番号一覧取得
+	 **/
+	public function getLotNumberListByOrder($prm = null) {
+		$prm = (object) $prm;
+		global $wpdb;
+		$cur_user = wp_get_current_user();
+		//var_dump($cur_user->user_login);
+		//var_dump($cur_user->user_email);
+
+		$sql  = "SELECT o.id, o.ship_addr, o.arrival_dt, o.name, g.goods, g.name as goods_name, g.qty as goods_qty, gd.lot, gd.tank ";
+		$sql .= "FROM yc_sales as o ";
+		$sql .= "LEFT JOIN yc_goods as g ON o.goods = g.goods ";
+		$sql .= "LEFT JOIN yc_goods_detail as gd on o.id = gd.order ";
+		$sql .= "WHERE o.id is not null ";
+		$sql .= "AND gd.id is not null ";
+		$sql .= sprintf("AND o.id = %d and g.goods = %d ", $prm->order, $prm->goods);
+
+		$rows = $wpdb->get_results($sql);
+
+//$this->vd($rows);
+		return $rows;
+	}
+
+	/**
+	 * 商品情報登録
+	 **/
+	public function regDetail($get = null, $post = null) {
+		global $wpdb;
+
+/*
+$wpdb->query($wpdb->prepare(
+"
+ INSERT INTO $wpdb->table_name
+ (column1, column2,column3)
+ VALUES(%d,%s,%s)
+ ON DUPLICATE KEY UPDATE
+ column2 = %s,
+ column3 = %s
+ ",
+$value1,
+$value2,
+$value3,
+$value4,
+$value5
+));
+*/
+
+		$exist_columns = $wpdb->get_col("DESC ". $this->getTableName(). ";", 0);
+		foreach ($exist_columns as $i => $col) {
+			if(!empty($post->$col)) {
+				$data[$col] = $post->$col;
+			}
+		}
+
+		$data['name'] = $post->goods_name;
+
+		$ret = $wpdb->insert(
+			$this->getTableName(), 
+			$data
+			//array('%s', '%s', '%d', '%s') // 第3引数: フォーマット
+		);
+
+		// 登録したIDを取得
+		$goods = $wpdb->insert_id;
+
+		// 登録情報を再取得
+		$rows = $this->getDetailByGoodsCode($goods);
+		return $rows;
+	}
+
+	/**
+	 * 商品情報更新
+	 **/
+	public function updDetail($get = null, $post = null) {
+		$post = (object) $post;
+		global $wpdb;
+
+		$exist_columns = $wpdb->get_col("DESC ". $this->getTableName(). ";", 0);
+		foreach ($exist_columns as $i => $col) {
+			if(!empty($post->$col)) {
+				$data[$col] = $post->$col;
+			}
+		}
+		$ret = $wpdb->update(
+			$this->getTableName(), 
+			$data, 
+			array('goods' => $post->goods)
+		);
+
+		// 更新情報を再取得
+		$rows = $this->getDetailByGoodsCode($post->goods);
+		return $rows;
+	}
+
+	/**
+	 * 
+	 **/
+	public function getInitForm() {
+		return array(
+			'select' => array(
+				'goods_name' => $this->getPartsGoodsName(), 
+			)
+		);
+	}
+
+	/**
+	 * 「品名」一覧用
+	 **/
+	private function getPartsGoodsName() {
+		global $wpdb;
+		$rows = $this->getList();
+
+		// 配列整形
+		foreach ($rows as $i => $d) {
+			if (!isset($d->name)) { continue; }
+			$ret[$d->goods] = sprintf("%s", $d->name);
+		}
+		return $ret;
+	}
+}
+?>
---
+++ views/stock-export.blade.php
@@ -0,0 +1,121 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+<div id="wpbody-content">
+	<div class="wrap">
+		<h1 class="wp-heading-inline">【在庫証明書】</h1>
+		<!--<a href="<?php echo home_url(); ?>/wp-admin/admin.php?page={{$formPage}}&action=regist" name="cmd_regist" id="cmd_regist" class="page-title-action">新規登録</a>-->
+		<!--<a href="<?php echo home_url(); ?>/wp-admin/admin.php?page=agreement" name="cmd_regist" id="cmd_regist" class="page-title-action">新規登録</a>-->
+
+		<hr class="wp-header-end">
+
+		<form name="forms" id="forms" action="" method="" enctype="multipart/form-data">
+{{--			@if ($tb->getCurUser()->roles[0] == 'administrator')	--}}
+			<div class="search-box">
+
+				<label for="carModel" class="col-sm-2 col-form-label">引取(入庫)予定日：</label>
+					<input type="date" id="user-search-input" name="s[arrival_s_dt]" value="<?php echo htmlspecialchars($get->s['arrival_s_dt']); ?>" placeholder="2020-11-01"><!--&emsp;～&emsp;
+				<input type="date" id="user-search-input" name="s[arrival_e_dt]" value="<?php echo htmlspecialchars($g['s']['arrival_e_dt']); ?>" placeholder="2022-12-01">&emsp;--><br /><br />
+
+				<label for="carModel" class="col-sm-2 col-form-label">出庫倉庫：</label>
+<!--					<input type="search" id="user-search-input" name="s[outgoing_warehouse]" value="<?php echo htmlspecialchars($get->s['outgoing_warehouse']); ?>">-->
+					<select class="" aria-label="outgoing_warehouse" id="outgoing_warehouse" name="s[outgoing_warehouse]">
+						@foreach($initForm['select']['outgoing_warehouse'] as $i => $d)
+							@if ($i == '0')
+							<option value=""></option>
+							@else
+							<option value="{{$i}}" @if ($i == $get->s['outgoing_warehouse']) selected @endif >{{$d}}</option>
+							@endif
+						@endforeach
+					</select>
+					&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+
+				<input type="button" id="search-submit" class="btn btn-primary" onclick="cmd_search();" value="検索">
+
+				<script>
+				function cmd_search() {
+					document.forms.method = 'get';
+					document.forms.action = "/wp-admin/admin.php?page=stock-receive&sales={{$get->sales}}&goods={{$get->goods}}&action=search"
+					document.forms.cmd.value = 'search';
+					document.forms.submit();
+				}
+				</script>
+			</div>
+			<br />
+
+			<input type="hidden" id="_wpnonce" name="_wpnonce" value="5647b2c250">
+			<!--<input type="hidden" name="_wp_http_referer" value="/wp-admin/users.php">-->
+			<input type="hidden" name="page" value="stock-receive">
+			<input type="hidden" name="action" value="search">
+			<input type="hidden" name="cmd" value="">
+{{--			@endif	--}}
+
+
+{{-- $wp_list_table->display() --}}
+<!-- start -->
+<div id="wpbody-content">
+	<div class="wrap">
+
+		<form method="get">
+			<div class="tablenav top">
+				<br class="clear">
+			</div>
+			
+		<div class="table-responsive">
+
+		<div>
+			<table class="table table-bordered text-nowrap">
+				<thead class="table-light">
+					<tr>
+						<th class="">No.</th>
+						<th class="">品名</th>
+						<th class="">量(t)</th>
+					</tr>
+				</thead>
+
+				@if (isset($rows) && count($rows))
+				<tbody id="the-list" data-wp-lists="list:user">
+					@foreach ($rows as $i => $row)
+					<tr id="">
+						<td class="">{{$i+1}}</td>
+						<td class="">{{$row->goods_name}}</td>
+						<td class="">{{$row->sum_qty}}</td>
+					</tr>
+					@endforeach
+				@else
+				<td class="colspanchange" colspan="7">検索対象は見つかりませんでした。</td>
+				@endif
+				</tbody>
+
+				<tfoot>
+				</tfoot>
+			</table>
+</div>
+<!-- end -->
+			<div class="tablenav top">
+				<br class="clear">
+			</div>
+			
+			<div class="tablenav bottom">
+				<br class="clear">
+			</div>
+		</form>
+		<div class="clear"></div>
+	</div>
+	<div class="clear"></div>
+</div>
+
+<script>
+function init_status(applicant = null) {
+	if (applicant == "" || applicant == null) {
+		alert("No. がありません。");
+		exit;
+	}
+
+	var str = "No. 【" + applicant + "】 の「登録状況」を初期化しますか？";
+	if (window.confirm(str)) {
+		//alert("初期化しました。");
+		location.href = location.protocol + "//" + location.hostname + "/wp-admin/admin.php?page=sales-list&post=" + applicant + "&action=init-status";
+	}
+}
+</script>
