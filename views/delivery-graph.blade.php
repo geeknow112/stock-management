1b7b5d6474d80966c93b8c4a1faece827514d2bf
przorzcrz <przorzcrz@gmail.com>
2024-01-08 16:56:43 +0000

    fix delivery-graph qty pulldown.

--- views/delivery-graph.blade.php
+++ views/delivery-graph.blade.php
@@ -362,9 +362,22 @@ function to_lot_regist(sales = null, goods = null) {
 						<option value="10.0">10.0</option>
 						<option value="12.5">12.5</option>
 				<?php } else { // その他 ?>
+<!--
 					<?php foreach ($initForm['select']['qty'] as $i => $qty) { ?>
 						<option value="<?php echo $i; ?>"><?php echo $qty; ?></option>
 					<?php } ?>
+-->
+						<option value=""></option>
+						<option value="0.5">0.5</option>
+						<option value="1.0">1.0</option>
+						<option value="1.5">1.5</option>
+						<option value="2.0">2.0</option>
+						<option value="2.5">2.5</option>
+						<option value="3.0">3.0</option>
+						<option value="3.5">3.5</option>
+						<option value="4.0">4.0</option>
+						<option value="4.5">4.5</option>
+						<option value="5.0">5.0</option>
 				<?php } ?>
 				</select>
 
