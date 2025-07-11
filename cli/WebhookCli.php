4b726ee5172c1cc5e6a78a7c86f8bf99a36419c6
geeknow112 <geeknow112@gmail.com>
2025-07-11 18:28:31 +0000

    Fixes geeknow112/srv-tools#199

--- cli/WebhookCli.php
+++ cli/WebhookCli.php
@@ -33,18 +33,64 @@ $cmd_ch_status = 'sudo /opt/bitnami/ctlscript.sh status';
 $ret = exec($cmd_ch_status);
 //var_dump($ret);exit;
 
-webhook_to_slack('test: yc watch. server runnning check OK.');
+$webhook_json = dirname(__DIR__). '/webhook.json';
+$webhooks = file_get_contents($webhook_json);
+$webhook = (object) json_decode($webhooks, true);
+
+//var_dump($webhook->yc2['url']);exit;
+
+$srv = array('yc2', 'keepa');
+
+foreach ($srv as $s) {
+	$ret = check_server($s, $webhook->$s['url']);
+	webhook_to_slack($s, (object) $webhook->$s, $ret);
+}
+
+/**
+ * check server
+ * 
+ **/
+function check_server($srv = null, $url) {
+	if (empty($url)) { return; }
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 10); // タイムアウト設定（秒）
+	$response = curl_exec($ch);
+	$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	$error = curl_error($ch);
+	$errno = curl_errno($ch);
+	curl_close($ch);
+
+	if ($errno) {
+		if ($errno == CURLE_OPERATION_TIMEOUTED) {
+			echo "Error: Timeout occurred. HTTP code: " . $httpCode . PHP_EOL;
+		} else {
+			echo "Error: " . $error . " (Error code: " . $errno . ")" . PHP_EOL;
+		}
+	} else {
+		echo "HTTP Status Code: " . $httpCode . PHP_EOL;
+
+		if ($httpCode == 200) {
+//			echo "取得成功: " . $response . PHP_EOL;
+			echo "取得成功: " . PHP_EOL;
+		} elseif ($httpCode == 504) {
+			echo "エラー: 504 Gateway Timeout" . PHP_EOL;
+		} else {
+			echo "その他のステータスコード: " . $httpCode . PHP_EOL;
+		}
+	}
+	return ($httpCode == 200) ? "OK" : "NG";
+}
 
 /**
  * webhook to slack
  * 
  **/
-function webhook_to_slack($str = null) {
+function webhook_to_slack($srv = null, $webhook = null, $msg = null) {
 	$dt = date('Y-m-d H:i:s');
-	$cmd = sprintf('curl -X POST --data \'{"text":"message from str.lober.work: %s. %s. "}\'', $dt, $str);
-	$webhook_json = dirname(__DIR__). '/webhook.json';
-	$webhooks = file_get_contents($webhook_json);
-	$webhook = (object) json_decode($webhooks, true);
+	$cmd = sprintf('curl -X POST --data \'{"text":"[%s] %s: %s. "}\'', $dt, $srv, $msg);
 	$slack = $webhook->slack;
 	exec($cmd. ' '. $slack);
 	return true;
